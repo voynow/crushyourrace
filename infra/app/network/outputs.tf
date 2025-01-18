e37b9b7268b56b5a1dbede970b907ca4959d57e5
voynow <voynow99@gmail.com>
2025-01-18 09:14:11 -0500

    goodbye ecs forever

--- infra/app/network/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc" {
-  value = aws_vpc.this
-}
-
-output "public_subnets" {
-  value = aws_subnet.public_subnets
-}
--- infra/setup/main.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_ecr_repository" "this" {
-  name = "${var.app_name}"
-}
